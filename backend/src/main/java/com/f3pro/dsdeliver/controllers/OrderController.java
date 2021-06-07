057a389017e719ac2e7efef1661e0c142356a4f1
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-06-07 17:08:46 -0300

    POST /orders

--- backend/src/main/java/com/f3pro/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/f3pro/dsdeliver/controllers/OrderController.java
@@ -1,27 +1,34 @@
 package com.f3pro.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.f3pro.dsdeliver.dto.OrderDTO;
 import com.f3pro.dsdeliver.services.OrderService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/orders")
 public class OrderController {
-	
+
     @Autowired
-    private OrderService  services;
+    private OrderService services;
 
     @GetMapping
     public ResponseEntity<List<OrderDTO>> findAll() {
 
         List<OrderDTO> list = services.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+        dto = services.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
