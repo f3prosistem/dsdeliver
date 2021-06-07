f2e0a3b269afb0518b2e10537ff8553f27f05624
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-06-07 17:49:13 -0300

    PUT /orders/{id} /delivered

--- backend/src/main/java/com/f3pro/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/f3pro/dsdeliver/controllers/OrderController.java
@@ -31,4 +31,12 @@ public class OrderController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping("/{id}/delivered")
+
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+        OrderDTO dto = services.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
 }
